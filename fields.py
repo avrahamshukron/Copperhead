545daa558e6a41874011c8ab150e52faa95ed34e
Avraham Shukron <avrahamshukron@users.noreply.github.com>
2016-06-27 22:04:41 +0300

    Completely reimagined fields
    Fields does not contain a value now, they simply provide a mechanism for
    encoding and decoding values.

--- fields.py
+++ fields.py
@@ -1,4 +1,3 @@
-import binascii
 import struct
 
 from validators import RangeValidator, MembershipValidator
@@ -7,51 +6,73 @@ LITTLE = "little"
 BIG = "big"
 
 
+class Encodable(object):
+
+    def encode(self):
+        """
+        Encode this object as a sequence of binary bytes
+        :return: This object encoded.
+        """
+        raise NotImplementedError("Abstract class")
+
+    def decode(self, buf):
+        """
+        Decode the value of this object from a sequence of bytes.
+
+        :param buf: A binary byte sequence
+        :return: The remainder of the buffer after consuming the data used by
+            this object.
+        """
+        raise NotImplementedError("Abstract class")
+
+
 class Field(object):
     """
     Base class representing Field that can be decoded/encoded
     """
 
-    DEFAULT_BYTE_ORDER = BIG
-
-    def __init__(self, initial_value, byte_order=DEFAULT_BYTE_ORDER):
-        self.value = initial_value
-        self.byte_order = byte_order
+    def __init__(self, **kwargs):
         self.validators = []
+
+    def get_default(self):
+        raise NotImplementedError("Abstract class")
 
     def validate(self, value):
         for validator in self.validators:
             validator.validate(value)
         return True
 
-    def encode(self):
+    def encode(self, value, out_buf):
         """
-        Encode this object as a byte string.
+        Encode ``value`` as a bytes into ``out_buf``.
 
-        :return: The encoded bytes representing the ``value`` attribute.
-        :raise ValueError: If the value cannot be encoded.
+        :param value: A value suitable for this object.
+        :param out_buf: A file-like object that has the ``write`` method.
+
+        :return: True if the operation succeeded.
+        :raise ValueError: If the value could not be encoded.
         """
-        if self.validate(self.value):
-            return self._encode()
+        if self.validate(value):
+            return self._encode(value, out_buf)
 
-    def _encode(self):
+    def _encode(self, value, out_buf):
         """
         Perform the actual encoding. Subclasses must implement.
         """
         raise NotImplementedError("Subclasses must implement")
 
-    def decode(self, byte_buf):
+    def decode(self, in_buf):
         """
-        Decode an value from a byte string, and sets it as its own.
+        Decode a value from a ``in_buf``.
 
-        :param byte_buf: A bytes-like object or an iterable producing bytes.
+        :param in_buf: A file-like object that has the ``read(int)`` method.
 
-        :return: The remaining buffer, without the data which was decoded.
+        :return: The the value that was decoded.
         :raise ValueError: If the data could not be decoded.
         """
-        rest = self._decode(byte_buf)
-        if self.validate(self.value):
-            return rest
+        value = self._decode(in_buf)
+        if self.validate(value):
+            return value
 
     def _decode(self, byte_buf):
         """
@@ -65,22 +86,27 @@ class Field(object):
 
 class UnsignedIntegerField(Field):
 
+    DEFAULT_BYTE_ORDER = BIG
     STANDARD_WIDTHS = {1: "B", 2: "H", 4: "I", 8: "Q"}
     ENDIAN = {BIG: ">", LITTLE: "<"}
     DEFAULT_WIDTH = 4
+    SIGNED = False
 
     @classmethod
     def get_bounds(cls, width):
         return 0, 2 ** (8 * width) - 1
 
-    def __init__(self, initial_value=0,
+    def __init__(self, default=0,
                  width=DEFAULT_WIDTH, min_value=None,
-                 max_value=None, **kwargs):
+                 max_value=None, byte_order=DEFAULT_BYTE_ORDER, **kwargs):
+        super(UnsignedIntegerField, self).__init__(**kwargs)
+
         if width not in self.STANDARD_WIDTHS:
             raise ValueError("Invalid width: %s. Supported widths are %s" %
                              (width, sorted(self.STANDARD_WIDTHS.keys())))
 
-        super(UnsignedIntegerField, self).__init__(initial_value, **kwargs)
+        self.default = default
+        self.byte_order = byte_order
         self.width = width
         self.min, self.max = self.get_bounds(self.width)
 
@@ -107,21 +133,23 @@ class UnsignedIntegerField(Field):
             # Python 3.2 and above. Best option. Best performance
             self._decode_func = self._decode_using_int
 
-    def _encode(self):
-        return self.struct.pack(self.value)
+    def get_default(self):
+        return self.default
 
-    def _decode(self, byte_buf):
-        if len(byte_buf) < self.width:
-            raise ValueError("Cannot decode - not enough data supplied")
+    def _encode(self, value, out_buf):
+        out_buf.write(self.struct.pack(value))
+        return True
 
-        mine = byte_buf[:self.width]
-        rest = byte_buf[self.width:]
-        self.value = self._decode_func(mine)
-        return rest
+    def _decode(self, in_buf):
+        mine = in_buf.read(self.width)
+        if not len or len(mine) < self.width:
+            raise ValueError("Cannot decode - reached end of data")
+
+        return self._decode_func(mine)
 
     def _decode_using_int(self, as_bytes):
         # noinspection PyUnresolvedReferences
-        return int.from_bytes(as_bytes, byteorder=self.byte_order, signed=False)
+        return int.from_bytes(as_bytes, byteorder=self.byte_order, signed=self.SIGNED)
 
     def _decode_using_struct(self, as_bytes):
         return self.struct.unpack(as_bytes)[0]
@@ -131,6 +159,7 @@ class SignedIntegerField(UnsignedIntegerField):
 
     STANDARD_WIDTHS = {1: "b", 2: "h", 4: "i", 8: "q"}
     ENDIAN = {BIG: ">", LITTLE: "<"}
+    SIGNED = True
 
     @classmethod
     def get_bounds(cls, width):
@@ -140,14 +169,17 @@ class SignedIntegerField(UnsignedIntegerField):
 
 class BooleanField(UnsignedIntegerField):
 
-    def __init__(self, initial_value=False):
+    def __init__(self, default=False, **kwargs):
         # BooleanField is a 1-byte integer
-        super(BooleanField, self).__init__(initial_value, width=1)
-        self._decode_func = lambda s: False if s == "\x00" else True
+        super(BooleanField, self).__init__(default, width=1, **kwargs)
 
-    def _encode(self):
+    def _encode(self, value, out_buf):
         # Optimized since we only have two possible values
-        return "\x01" if self.value else "\x00"
+        out_buf.write("\x01" if value else "\x00")
+
+    @staticmethod
+    def _decode_func(as_bytes):
+        return False if as_bytes == "\x00" else True
 
 
 class Enum(object):
@@ -167,8 +199,7 @@ class EnumField(UnsignedIntegerField):
 
     DEFAULT_WIDTH = 1
 
-    def __init__(self, values, **kwargs):
-        width = kwargs.pop("width", self.DEFAULT_WIDTH)
+    def __init__(self, values, width=DEFAULT_WIDTH, **kwargs):
         super(EnumField, self).__init__(width=width, **kwargs)
         self.values = Enum(values)
         self.validators.append(MembershipValidator(self.values))
--- tests/field_tests.py
+++ tests/field_tests.py
@@ -2,6 +2,8 @@ from unittest import TestCase
 
 import struct
 
+from cStringIO import StringIO
+
 from fields import BooleanField, UnsignedIntegerField, SignedIntegerField, \
     EnumField, Enum
 
@@ -15,20 +17,25 @@ class UnsignedIntegerFieldTests(TestCase):
         for width in (-1, 0, 3, 5, 6, 7, 9, 10):
             self.failUnlessRaises(ValueError, UnsignedIntegerField, width=width)
 
+    def test_get_default(self):
         for width in UnsignedIntegerField.STANDARD_WIDTHS.keys():
-            initial_value = 124
-            f = UnsignedIntegerField(width=width, initial_value=initial_value)
+            default = 124
+            f = UnsignedIntegerField(width=width, default=default)
             self.failUnlessEqual(f.width, width)
             self.failUnlessEqual(
-                f.value, initial_value, msg="Wrong value observed")
+                f.get_default(), default, msg="Wrong value observed")
 
     def test_class_bounds(self):
         for width in UnsignedIntegerField.STANDARD_WIDTHS.keys():
             for value in (-1, 2 ** (8 * width)):
-                f = UnsignedIntegerField(width=width, initial_value=value)
-                self.failUnlessRaises(ValueError, f.encode)
-                f.decode("\x00" * width)
-                f.decode("\xff" * width)
+                empty = StringIO()
+                f = UnsignedIntegerField(width=width)
+                self.failUnlessRaises(ValueError, f.encode, value, empty)
+                self.failUnlessEqual(f.decode(StringIO("\x00" * width)), 0)
+                self.failUnlessEqual(
+                    f.decode(StringIO("\xff" * width)),
+                    2 ** (8 * width) - 1
+                )
 
     def test_user_bounds(self):
         max_value = 123456
@@ -36,28 +43,30 @@ class UnsignedIntegerFieldTests(TestCase):
         unlimited = UnsignedIntegerField()
         limited = UnsignedIntegerField(min_value=min_value, max_value=max_value)
         for value in (0, min_value - 1, max_value + 1, 999999):
-            limited.value = value
-            self.failUnlessRaises(ValueError, limited.encode)
-            unlimited.value = value
-            encoded = unlimited.encode()
-            self.failUnlessRaises(ValueError, limited.decode, encoded)
+            buf_in = StringIO()
+            self.failUnlessRaises(ValueError, limited.encode, value, buf_in)
+            unlimited.encode(value, buf_in)
+            self.failUnlessRaises(ValueError, limited.decode, buf_in)
 
     def test_encoding(self):
         for width in UnsignedIntegerField.STANDARD_WIDTHS.keys():
             expected = "\x0f" * width
             value = reduce(lambda x, y: (x << 8) + y, [0x0f] * width, 0)
             print hex(value)
-            f = UnsignedIntegerField(width=width, initial_value=value)
-            self.failUnlessEqual(f.encode(), expected)
+            out_buf = StringIO()
+            f = UnsignedIntegerField(width=width)
+            f.encode(value, out_buf)
+            self.failUnlessEqual(out_buf.getvalue(), expected)
 
     def test_decoding(self):
         for width in UnsignedIntegerField.STANDARD_WIDTHS.keys():
-            encoded = "\x0f" * width
+            encoded = StringIO("\x0f" * width)
             value = reduce(lambda x, y: (x << 8) + y, [0x0f] * width, 0)
             f = UnsignedIntegerField(width=width)
-            remaining = f.decode(encoded)
-            self.failUnlessEqual(f.value, value, msg="Incorrect decoded value")
-            self.failIf(remaining, msg="Incorrect remaining data")
+            decoded = f.decode(encoded)
+            self.failUnlessEqual(decoded, value, msg="Incorrect decoded value")
+            self.failUnlessEqual(
+                encoded.read(), "", msg="Incorrect remaining data")
 
 
 class SignedIntegerTests(TestCase):
@@ -68,26 +77,28 @@ class SignedIntegerTests(TestCase):
             lower_bound = -2 ** value_bits
             upper_bound = 2 ** value_bits - 1
             for value in (lower_bound - 1, upper_bound + 1):
-                f = SignedIntegerField(width=width, initial_value=value)
-                self.failUnlessRaises(ValueError, f.encode)
+                f = SignedIntegerField(width=width)
+                out_buf = StringIO()
+                self.failUnlessRaises(ValueError, f.encode, value, out_buf)
 
     def test_encoding_decoding(self):
         for width in SignedIntegerField.STANDARD_WIDTHS.keys():
             f = SignedIntegerField(width=width)
-            for value, encoded in ((-1, "\xff"), (-127, "\x81"),
-                                   (0, "\x00"), (1, "\x01"),
-                                   (127, "\x7f"),):
-                f.value = value
+            for value, expected_encoding in ((-1, "\xff"), (-127, "\x81"),
+                                             (0, "\x00"), (1, "\x01"),
+                                             (127, "\x7f"),):
                 padding = "\xff" if value < 0 else "\x00"
-                encoded = encoded.rjust(width, padding)
-                self.failUnlessEqual(f.encode(), encoded)
+                expected_encoding = expected_encoding.rjust(width, padding)
+                out_buf = StringIO()
+                f.encode(value, out_buf)
+                encoded = out_buf.getvalue()
+                self.failUnlessEqual(encoded, expected_encoding)
 
-                # Flip sign just to make sure that the next test won't just have
-                # the correct value from before.
-                f.value *= -1
-                remaining = f.decode(encoded)
-                self.failIf(remaining, msg="Incorrect remaining buffer")
-                self.failUnlessEqual(f.value, value)
+                out_buf.reset()
+                decoded_value = f.decode(out_buf)
+                self.failUnlessEqual(
+                    out_buf.read(), "", msg="Incorrect remaining buffer")
+                self.failUnlessEqual(decoded_value, value)
 
 
 class BooleanFieldTests(TestCase):
@@ -97,18 +108,24 @@ class BooleanFieldTests(TestCase):
         for value, expected in (
                 (True, "\x01"), (False, "\x00"),
                 (0, "\x00"), (1, "\x01"), (255, "\x01")):
-            f.value = value
-            self.assertEqual(f.encode(), expected)
+            out_buf = StringIO()
+            f.encode(value, out_buf)
+            self.assertEqual(out_buf.getvalue(), expected)
 
     def test_decoding(self):
         f = BooleanField()
-        for value, encoded in ((True, "\x01\xff\xff"), (False, "\x00\xff\xff")):
-            rest = f.decode(encoded)
+        for expected_value, encoded in ((True, "\x01\xff\xff"),
+                                        (False, "\x00\xff\xff")):
+            buf_in = StringIO(encoded)
+            decoded_value = f.decode(buf_in)
             # Check that the remaining data was not consumed.
+            self.failUnlessEqual(
+                buf_in.read(), encoded[f.width:],
+                msg="Remaining data incorrect")
+            buf_in.close()
+            # Check that the expected_value is correct
             self.assertEqual(
-                rest, encoded[f.width:], msg="Remaining data incorrect")
-            # Check that the value is correct
-            self.assertEqual(f.value, value, msg="Decoded value incorrect")
+                decoded_value, expected_value, msg="Incorrect decoded value")
 
 
 class EnumFieldTests(TestCase):
@@ -126,15 +143,16 @@ class EnumFieldTests(TestCase):
     def test_encode(self):
         e = EnumField(values=self.VALUES)
         for value in self.numeric_values:
-            e.value = value
-            encoded = e.encode()
-            self.failUnlessEqual(encoded, self.struct.pack(value),
+            out_buf = StringIO()
+            e.encode(value, out_buf)
+            self.failUnlessEqual(out_buf.getvalue(), self.struct.pack(value),
                                  msg="Incorrect encoded value")
+            out_buf.close()
 
         not_members = set(range(1, 20)) - set(self.numeric_values)
         for value in not_members:
-            e.value = value
-            self.failUnlessRaises(ValueError, e.encode)
+            out_buf = StringIO()
+            self.failUnlessRaises(ValueError, e.encode, value, out_buf)
 
     def test_decode(self):
         e = EnumField(values=self.VALUES)
@@ -142,13 +160,15 @@ class EnumFieldTests(TestCase):
         for encoded, value in (
                 (self.struct.pack(v), v)
                 for v in self.numeric_values):
-            e.decode(encoded)
-            self.failUnlessEqual(e.value, value, msg="Incorrect decoded value")
+            in_buf = StringIO(encoded)
+            decoded_value = e.decode(in_buf)
+            self.failUnlessEqual(
+                decoded_value, value, msg="Incorrect decoded value")
 
         not_members = set(range(1, 20)) - set(self.numeric_values)
-
         for encoded in (self.struct.pack(v) for v in not_members):
-            self.failUnlessRaises(ValueError, e.decode, encoded)
+            in_buf = StringIO(encoded)
+            self.failUnlessRaises(ValueError, e.decode, in_buf)
 
     def test_enum_api(self):
         e = Enum(self.VALUES)

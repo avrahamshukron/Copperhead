e87d73de9758feebce01394777f89933e27c4ea6
Avraham Shukron <avraham.shukron@gmail.com>
2016-07-06 07:27:25 +0300

    Container tests: Added dummy containers for tests cases

--- tests/dummy.py
+++ tests/dummy.py
@@ -1,1 +1,49 @@
-__author__ = "avraham.shukron@gmail.com"
+from containers import Record, Choice
+from primitives import UnsignedInteger, ByteOrder
+
+
+class GetStatus(Record):
+    order = ()
+
+
+class Upgrade(Record):
+    order = ()
+
+
+class Reset(Record):
+    order = ()
+
+
+class GeneralCommands(Choice):
+    variants = {
+        0x00: GetStatus,
+        0x02: Reset
+    }
+
+
+class UpgradeCommands(Choice):
+    variants = {
+        0x00: Upgrade,
+    }
+
+
+class Command(Choice):
+    variants = {
+        0x00: GeneralCommands,
+        0x01: UpgradeCommands,
+    }
+
+
+class Header(Record):
+    barker = UnsignedInteger(
+        default=0xcafebeef, byte_order=ByteOrder.BIG, width=4)
+    size = UnsignedInteger(default=0, byte_order=ByteOrder.BIG, width=2)
+    inverted_size = size
+    order = ("barker", "size", "inverted_size")
+
+
+class Packet(Record):
+    header = Header
+    payload = Command
+    crc = UnsignedInteger(width=4, byte_order=ByteOrder.BIG)
+    order = ("header", "payload", "crc")

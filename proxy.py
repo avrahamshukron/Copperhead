50a6d8ccc38e904f89c9fddeb83cccda1d1d809e
Avraham Shukron <avraham.shukron@gmail.com>
2016-07-10 00:07:24 +0300

    Proxy: Added comparison support
    And replaced any use of ‘ with “

--- proxy.py
+++ proxy.py
@@ -50,24 +50,27 @@ class Proxy(object):
     def __hash__(self):
         return hash(object.__getattribute__(self, "_obj"))
 
+    def __eq__(self, other):
+        return object.__getattribute__(self, "_obj") == other
+
     #
     # Factories
     #
     _special_names = [
-        '__abs__', '__add__', '__and__', '__call__', '__cmp__', '__coerce__',
-        '__contains__', '__delitem__', '__delslice__', '__div__', '__divmod__',
-        '__eq__', '__float__', '__floordiv__', '__ge__', '__getslice__',
-        '__gt__', '__hex__', '__iadd__', '__iand__', '__idiv__', '__idivmod__',
-        '__ifloordiv__', '__ilshift__', '__imod__', '__imul__', '__int__',
-        '__invert__', '__ior__', '__ipow__', '__irshift__', '__isub__',
-        '__iter__', '__itruediv__', '__ixor__', '__le__','__long__',
-        '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__',
-        '__oct__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__',
-        '__rdiv__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__',
-        '__reversed__', '__rfloorfiv__', '__rlshift__', '__rmod__', '__rmul__',
-        '__ror__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__',
-        '__rtruediv__', '__rxor__', '__setitem__', '__setslice__', '__sub__',
-        '__truediv__', '__xor__', 'next',
+        "__abs__", "__add__", "__and__", "__call__", "__cmp__", "__coerce__",
+        "__contains__", "__delitem__", "__delslice__", "__div__", "__divmod__",
+        "__float__", "__floordiv__", "__ge__", "__getslice__",
+        "__gt__", "__hex__", "__iadd__", "__iand__", "__idiv__", "__idivmod__",
+        "__ifloordiv__", "__ilshift__", "__imod__", "__imul__", "__int__",
+        "__invert__", "__ior__", "__ipow__", "__irshift__", "__isub__",
+        "__iter__", "__itruediv__", "__ixor__", "__le__","__long__",
+        "__lshift__", "__lt__", "__mod__", "__mul__", "__ne__", "__neg__",
+        "__oct__", "__or__", "__pos__", "__pow__", "__radd__", "__rand__",
+        "__rdiv__", "__rdivmod__", "__reduce__", "__reduce_ex__", "__repr__",
+        "__reversed__", "__rfloorfiv__", "__rlshift__", "__rmod__", "__rmul__",
+        "__ror__", "__rpow__", "__rrshift__", "__rshift__", "__rsub__",
+        "__rtruediv__", "__rxor__", "__setitem__", "__setslice__", "__sub__",
+        "__truediv__", "__xor__", "next",
     ]
 
     @classmethod
@@ -96,7 +99,7 @@ class Proxy(object):
     def __new__(cls, obj, *args, **kwargs):
         """
         creates an proxy instance referencing `obj`. (obj, *args, **kwargs) are
-        passed to this class' __init__, so deriving classes can define an
+        passed to this class" __init__, so deriving classes can define an
         __init__ method of their own.
         note: _class_proxy_cache is unique per deriving class (each deriving
         class must hold its own cache)

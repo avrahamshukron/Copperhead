53f4ffb3d71b575749f8c794297f2f1013e870f4
Avraham Shukron <avraham.shukron@gmail.com>
2017-04-28 19:19:56 +0300

    Added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="ProtoPy",
+    version="0.1",
+    packages=["tests", "protopy"],
+    url="",
+    license="",
+    author="Avraham Shukron",
+    author_email="",
+    description="A framework for describing, encoding and decoding binary "
+                "structures"
+)
